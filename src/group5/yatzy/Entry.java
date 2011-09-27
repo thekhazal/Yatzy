b6456b67181840fb2735b62537abee99703ea4a7
Emma Bogren <boemma@student.chalmers.se>
2011-09-27 11:15:15 +0200

    Test

---
+++ src/group5/yatzy/Entry.java
@@ -0,0 +1,53 @@
+/*
+* Entry är en klass som representerar
+* en avbildning med en nyckel och ett värde.
+*/
+	public class Entry<K,V>
+	{
+		protected K key;
+		protected V value;
+
+		
+		/*
+		 * Konstruktorn tar in en nyckel och
+		 * ett värde.
+		 */
+		public Entry (K k,V v)
+		{
+			key = k;
+			value = v;
+		}
+		
+		/*
+		 * Returnerar nyckeln för avbildningen.
+		 */
+		public K getKey()
+		{
+			return key;
+		}
+		
+		/*
+		 * Returnerar värdet för avbildningen.
+		 */ 
+		public V getValue()
+		{
+			return value;
+		}
+		
+
+		/*
+		 * Sätter avbildningens värde.
+		 */ 
+		public void setValue(V v)
+		{
+			this.value = v;
+		}
+		
+		/*
+		 * Returnerar en strängrepresentation av klassen.
+		 */
+		public String toString()
+		{
+			return key + " " + value;
+		}
+	}
---
+++ src/group5/yatzy/HighScore.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class HighScore {
+
+	ArrayList<Entry<String,Integer>> winners;
+	
+	public HighScore()
+	{
+		winners = new ArrayList<Entry<String,Integer>>();
+	}
+	
+	public ArrayList<Entry<String,Integer>> getWinners()
+	{
+		return winners;
+	}
+	
+	public int update(String name,int score)
+	{
+		for(Entry e:winners)
+		{
+			if()
+		}
+	
+	}
+	
+}
