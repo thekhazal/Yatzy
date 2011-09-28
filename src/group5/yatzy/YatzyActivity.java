257e21c5a2bc68b5de33142e000fcc49d11e04d5
Anmar Khazal <anmar_khazal@hotmail.com>
2011-09-28 16:01:48 +0200

    git working with single file upload but not event ONCLICK on buttons

--- src/group5/yatzy/YatzyActivity.java
+++ src/group5/yatzy/YatzyActivity.java
@@ -9,18 +9,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class YatzyActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
-    private OnClickListener mCorkyListener = new OnClickListener() {
-        public void onClick(View v) {
-          System.out.print("HEJ");
-        }
-    };
+
+
+	private OnClickListener mCorkyListener = new OnClickListener() {
+		public void onClick(View v) {
+			System.out.println("HEJ");
+		}
+	};
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		Button button1 = (Button)findViewById(R.id.resumegame);
+		Button button2 = (Button)findViewById(R.id.newgame);
+		Button button3 = (Button)findViewById(R.id.highscore);
+		button1.setClickable(true);
+		button1.setOnClickListener(mCorkyListener);
+		button2.setOnClickListener(mCorkyListener);
+		button3.setOnClickListener(mCorkyListener);
+	}
+
+
 }
