3b149c3f54db6fd6bfa4c8fff340b005718eacb2
Anmar Khazal <anmar_khazal@hotmail.com>
2011-09-28 15:02:29 +0200

    temp save

--- src/group5/yatzy/YatzyActivity.java
+++ src/group5/yatzy/YatzyActivity.java
@@ -1,7 +1,14 @@
 package group5.yatzy;
 
+/**
+ * @author Anmar Khazal
+ * This activity class will display the main menu
+ * with three buttons, resume game, new game and high score
+ **/
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class YatzyActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +17,10 @@ public class YatzyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    private OnClickListener mCorkyListener = new OnClickListener() {
+        public void onClick(View v) {
+          System.out.print("HEJ");
+        }
+    };
 }
