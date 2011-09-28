3c18fd732299f0f52922bf5c78d00869bf2d40d5
Emma Bogren <boemma@student.chalmers.se>
2011-09-28 16:24:21 +0200

    Lagt till highscoreknappen

--- src/group5/yatzy/YatzyActivity.java
+++ src/group5/yatzy/YatzyActivity.java
@@ -1,39 +1,31 @@
-
 package group5.yatzy;
 
-/**
- * @author Anmar Khazal
- * This activity class will display the main menu
- * with three buttons, resume game, new game and high score
- **/
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class YatzyActivity extends Activity {
-
-
-	private OnClickListener mCorkyListener = new OnClickListener() {
-		public void onClick(View v) {
-			System.out.println("HEJ");
-		}
-	};
-
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		Button button1 = (Button)findViewById(R.id.resumegame);
-		Button button2 = (Button)findViewById(R.id.newgame);
-		Button button3 = (Button)findViewById(R.id.highscore);
-		button1.setClickable(true);
-		button1.setOnClickListener(mCorkyListener);
-		button2.setOnClickListener(mCorkyListener);
-		button3.setOnClickListener(mCorkyListener);
-	}
-
-
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        /////highscore
+        HighScore hs = new HighScore();
+        Button hsButton = (Button) findViewById(R.id.highscore);
+        hsButton.setOnClickListener(new OnClickListener(){
+        	public void onClick(View v) {
+        		
+        		Intent gameIntent = new Intent(YatzyActivity.this,HighScoreActivity.class);
+        		startActivity(gameIntent);
+        	}
+        }); 
+        //////
+    }
 }
