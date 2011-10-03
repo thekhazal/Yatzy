c7590e15718826ca23ecfee7e00e93a8d977aee4
Viktor Swantesson <viktorswantesson@hotmail.com>
2011-10-03 19:42:03 +0200

    Updated the names of the .gifs in drawable-hdpi

--- gen/group5/yatzy/R.java
+++ gen/group5/yatzy/R.java
@@ -16,12 +16,18 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int five=0x7f020001;
-        public static final int four=0x7f020002;
-        public static final int icon=0x7f020003;
-        public static final int one=0x7f020004;
-        public static final int six=0x7f020005;
-        public static final int three=0x7f020006;
-        public static final int two=0x7f020007;
+        public static final int fiveheld=0x7f020002;
+        public static final int four=0x7f020003;
+        public static final int fourheld=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int one=0x7f020006;
+        public static final int oneheld=0x7f020007;
+        public static final int six=0x7f020008;
+        public static final int sixheld=0x7f020009;
+        public static final int three=0x7f02000a;
+        public static final int threeheld=0x7f02000b;
+        public static final int two=0x7f02000c;
+        public static final int twoheld=0x7f02000d;
     }
     public static final class id {
         public static final int bigStraight=0x7f06002e;
