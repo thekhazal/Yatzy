27cda34c1a9b8c04edb7d5fb8669ac8d7204d04a
Anmar Khazal <anmar_khazal@hotmail.com>
2011-10-03 10:50:34 +0200

    test

--- gen/group5/yatzy/R.java
+++ gen/group5/yatzy/R.java
@@ -14,27 +14,44 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int five=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
+        public static final int clear=0x7f06000e;
+        public static final int five=0x7f060001;
         public static final int gameText=0x7f060000;
-        public static final int highscore=0x7f060003;
-        public static final int newgame=0x7f060002;
-        public static final int nrOfPlayers=0x7f060006;
-        public static final int nrOfPlayersText=0x7f060007;
-        public static final int player1=0x7f060008;
-        public static final int player2=0x7f060009;
-        public static final int player3=0x7f06000a;
-        public static final int player4=0x7f06000b;
-        public static final int resumegame=0x7f060001;
-        public static final int selectPlayersText=0x7f060005;
-        public static final int startButton=0x7f06000c;
-        public static final int startnewgametext=0x7f060004;
+        public static final int highscore=0x7f060011;
+        public static final int linearLayout1=0x7f060003;
+        public static final int name1=0x7f060009;
+        public static final int name2=0x7f06000b;
+        public static final int name3=0x7f06000d;
+        public static final int newgame=0x7f060010;
+        public static final int no=0x7f060005;
+        public static final int nrOfPlayers=0x7f060014;
+        public static final int nrOfPlayersText=0x7f060015;
+        public static final int player1=0x7f060016;
+        public static final int player2=0x7f060017;
+        public static final int player3=0x7f060018;
+        public static final int player4=0x7f060019;
+        public static final int resumegame=0x7f06000f;
+        public static final int selectPlayersText=0x7f060013;
+        public static final int startButton=0x7f06001a;
+        public static final int startnewgametext=0x7f060012;
+        public static final int sureText=0x7f060002;
+        public static final int tableLayout1=0x7f060006;
+        public static final int tableRow1=0x7f060007;
+        public static final int tableRow2=0x7f060008;
+        public static final int tableRow3=0x7f06000a;
+        public static final int tableRow4=0x7f06000c;
+        public static final int yes=0x7f060004;
     }
     public static final class layout {
         public static final int game=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int newgame=0x7f030002;
+        public static final int highdialog=0x7f030001;
+        public static final int highscore=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int newgame=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
@@ -42,6 +59,7 @@ public final class R {
         public static final int game=0x7f04000c;
         public static final int hello=0x7f040000;
         public static final int highscore=0x7f040004;
+        public static final int highwarning=0x7f04000d;
         public static final int names=0x7f04000b;
         public static final int newgame=0x7f040003;
         public static final int nrofplayers=0x7f04000a;
@@ -50,5 +68,6 @@ public final class R {
         public static final int players_prompt=0x7f040008;
         public static final int resumegame=0x7f040002;
         public static final int startgame=0x7f040009;
+        public static final int winner_is=0x7f04000e;
     }
 }
