f8b9b26ef28e8afe607544fcd457d94c84a446d3
swante <viktorswantesson@hotmail.com>
2011-10-03 20:33:46 +0300

    Updated so that player cannot hold any of the dice before the first toss. Can not toss if all dice are being held at once. Can NOT toss if it has been the last throw turn of that specific player. Although at the moment only one player is available and you can only throw the dice three times/app start.
    Also added some minor methods for updating dice and to see if a dice is being held or not.

--- src/group5/yatzy/GameActivity.java
+++ src/group5/yatzy/GameActivity.java
@@ -11,6 +11,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+/**
+ * This class controls the game flow and updates the game.xml. 
+ * @author Daniel Gunnarsson, Viktor Swantesson
+ *
+ */
 public class GameActivity extends Activity {
 
 	ArrayList<Dice> dice = new ArrayList<Dice>();
@@ -19,8 +24,9 @@ public class GameActivity extends Activity {
 	ArrayList<Player> players = new ArrayList<Player>();
 	int roundNr = 1;
 	int playerTurn = 1;
-	int throwTurn = 1;
-	
+	int throwTurn = 0;
+	int diceBeingHeld = 0;
+
 	TextView gameText;
 	TextView playerName;
 	TextView ones;
@@ -48,27 +54,13 @@ public class GameActivity extends Activity {
 	ImageView dice4;
 	ImageView dice5;
 	Button throwButton;
-	
+
 	/** Called when the activity is first created.*/
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game);
-		
-		for(int i = 0; i < 5; i++){
-			dice.add(new Dice());
-		}
-		
-		/**
-		* List with names of players to start game withf
-		 */
-		playerNames = getIntent().getStringArrayListExtra("NewGameActivity");
-		
-		
-		for(int i = 0; i < playerNames.size()-1; i++){
-			players.add(new Player(playerNames.get(i)));
-		}
-		
+
 		gameText 	= (TextView) 	findViewById(R.id.headText);
 		playerName 	= (TextView) 	findViewById(R.id.playerNameText);
 		ones 		= (TextView) 	findViewById(R.id.ones);
@@ -96,13 +88,39 @@ public class GameActivity extends Activity {
 		dice4		= (ImageView)	findViewById(R.id.dice4);
 		dice5		= (ImageView)	findViewById(R.id.dice5);
 		throwButton = (Button)		findViewById(R.id.throwButton);
+
+		/**
+		 * Creates the five dice.
+		 */
+		for(int i = 0; i < 5; i++){
+			dice.add(new Dice());
+		}
 		
+		/**
+		 * The dice views are being added to the diceImages list.
+		 */
 		diceImages.add(dice1);
 		diceImages.add(dice2);
 		diceImages.add(dice3);
 		diceImages.add(dice4);
 		diceImages.add(dice5);
-		
+
+		/**
+		 * List with names of players to start game withf
+		 */
+		playerNames = getIntent().getStringArrayListExtra("NewGameActivity");
+
+		/**
+		 * Puts all the players in the "players"-list.
+		 */
+		for(int i = 0; i < playerNames.size()-1; i++){
+			players.add(new Player(playerNames.get(i)));
+		}
+
+		/**
+		 * Only happens once when the game is started for the first time to
+		 * give all the five dice random numbers when started.
+		 */
 		for(int i = 0; i < 5; i++){
 			if(dice.get(i).getValue() == 1)
 				diceImages.get(i).setImageResource(R.drawable.one);
@@ -117,10 +135,15 @@ public class GameActivity extends Activity {
 			if(dice.get(i).getValue() == 6)
 				diceImages.get(i).setImageResource(R.drawable.six);
 		}
-		
+
+		/**
+		 * Listener for the throw button. Checks if a dice is being held or not
+		 * before throwing a dice. If held nothing happens. After each 
+		 */
 		throwButton.setOnClickListener(new OnClickListener(){
 			public void onClick(View v) {
 				for(int i = 0; i < dice.size(); i++){
+
 					if(!dice.get(i).isHeld()){
 						dice.get(i).roll();
 						updateDice(dice.get(i), i);
@@ -129,67 +152,125 @@ public class GameActivity extends Activity {
 				nextThrow();
 			}
 		});
-		
+
+		/**
+		 * Listeners for all the dice. Marks if a dice will be held/not held
+		 * when clicked.
+		 */
 		dice1.setOnClickListener(new OnClickListener(){
 			public void onClick(View v) {
-				
-				if(!dice.get(0).isHeld()){
-					dice.get(0).setHeld(true);
-					diceImages.get(0).setImageResource(R.drawable.one);
+				if(throwTurn == 1 || throwTurn == 2){
+					updateHeldStatus(dice.get(0));
+					updateDice(dice.get(0), 0);
 				}
-				
-				if(dice.get(0).isHeld()){
-					dice.get(0).setHeld(false);
-					diceImages.get(0).setImageResource(R.drawable.icon);
+			}
+		});
+		dice2.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				if(throwTurn == 1 || throwTurn == 2){
+					updateHeldStatus(dice.get(1));
+					updateDice(dice.get(1), 1);
+				}
+			}
+		});
+		dice3.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				if(throwTurn == 1 || throwTurn == 2){
+					updateHeldStatus(dice.get(2));
+					updateDice(dice.get(2), 2);
+				}
+			}
+		});
+		dice4.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				if(throwTurn == 1 || throwTurn == 2){
+					updateHeldStatus(dice.get(3));
+					updateDice(dice.get(3), 3);
+				}
+			}
+		});
+		dice5.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				if(throwTurn == 1 || throwTurn == 2){
+					updateHeldStatus(dice.get(4));
+					updateDice(dice.get(4), 4);
 				}
 			}
 		});
 		//CharSequence as = (CharSequence) playerNames.get(0);
 		//test.setText(as);
 	}
-	
-	public void updateDice(Dice dice, int diceNr) {
+
+	/**
+	 * This method updates a dice current holding status with the inverse
+	 * boolean value.
+	 * @param dice The die that will be updated.
+	 */
+	private void updateHeldStatus(Dice dice) {
+		dice.setHeld(!dice.isHeld());
+
+		if(dice.isHeld()){
+			diceBeingHeld++;
+
+			if(diceBeingHeld >= 5)
+				throwButton.setClickable(false);
+		}
+		else {
+			diceBeingHeld--;
+			throwButton.setClickable(true);
+		}
+	}
+
+	/**
+	 * This method updates the dice on the screen to its correct dice status
+	 * (held/not held).
+	 * @param dice The dice that will be updated.
+	 * @param diceNr The number of that specific dice in the diceImages list.
+	 */
+	private void updateDice(Dice dice, int diceNr) {
+		// For a dice that will be unheld
 		if(!dice.isHeld()){
+
 			if(dice.getValue() == 1)
 				diceImages.get(diceNr).setImageResource(R.drawable.one);
-			
+
 			if(dice.getValue() == 2)
 				diceImages.get(diceNr).setImageResource(R.drawable.two);
-			
+
 			if(dice.getValue() == 3)
 				diceImages.get(diceNr).setImageResource(R.drawable.three);
-			
+
 			if(dice.getValue() == 4)
 				diceImages.get(diceNr).setImageResource(R.drawable.four);
-			
+
 			if(dice.getValue() == 5)
 				diceImages.get(diceNr).setImageResource(R.drawable.five);
-			
+
 			if(dice.getValue() == 6)
 				diceImages.get(diceNr).setImageResource(R.drawable.six);
 		}
-		//SÄTT RÖD RAM RUNT PÅ ELSE!
+		// For a dice that will be held
 		else {
 			if(dice.getValue() == 1)
-				diceImages.get(diceNr).setImageResource(R.drawable.icon);
-			
+				diceImages.get(diceNr).setImageResource(R.drawable.oneheld);
+
 			if(dice.getValue() == 2)
-				diceImages.get(diceNr).setImageResource(R.drawable.icon);
-			
+				diceImages.get(diceNr).setImageResource(R.drawable.twoheld);
+
 			if(dice.getValue() == 3)
-				diceImages.get(diceNr).setImageResource(R.drawable.icon);
-			
+				diceImages.get(diceNr).setImageResource(R.drawable.threeheld);
+
 			if(dice.getValue() == 4)
-				diceImages.get(diceNr).setImageResource(R.drawable.icon);
-			
+				diceImages.get(diceNr).setImageResource(R.drawable.fourheld);
+
 			if(dice.getValue() == 5)
-				diceImages.get(diceNr).setImageResource(R.drawable.icon);
-			
+				diceImages.get(diceNr).setImageResource(R.drawable.fiveheld);
+
 			if(dice.getValue() == 6)
-				diceImages.get(diceNr).setImageResource(R.drawable.icon);
+				diceImages.get(diceNr).setImageResource(R.drawable.sixheld);
 		}
 	}
-	
+
 	public int roundCheck() {
 		return roundNr;
 	}
@@ -200,9 +281,14 @@ public class GameActivity extends Activity {
 			playerTurn = 1;
 	}
 
+	/**
+	 * This method increases the current throw turn. If the throw turn is equal
+	 * to three the throw button will be unclickable.
+	 */
 	public void nextThrow() {
-		if(throwTurn == 3)
-			throwTurn = 0;
 		throwTurn++;
+
+		if(throwTurn == 3)
+			throwButton.setClickable(false);
 	}
 }
