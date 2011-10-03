240f88466eb043f35944042ef2b42bfa0a313de7
Daniel Gunnarsson <gsn.daniel@gmail.com>
2011-10-03 14:57:46 +0200

    Updated GameActivity.java

--- src/group5/yatzy/GameActivity.java
+++ src/group5/yatzy/GameActivity.java
@@ -1,22 +1,28 @@
 package group5.yatzy;
 
 
-import java.io.Serializable;
 import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class GameActivity extends Activity {
 
+	ArrayList<Dice> dice;
+	ArrayList<ImageView> diceImages;
 	ArrayList<String> playerNames;
 	ArrayList<Player> players;
 	int roundNr = 1;
 	int playerTurn = 1;
 	int throwTurn = 1;
 	
+	TextView gameText;
+	TextView playerName;
 	TextView ones;
 	TextView twos;
 	TextView threes;
@@ -26,15 +32,22 @@ public class GameActivity extends Activity {
 	TextView sum;
 	TextView bonus;
 	TextView onePair;
-	TextView twoPair;
+	TextView twoPairs;
 	TextView trips;
 	TextView quads;
-	TextView smallStraight;
-	TextView bigStraight;
+	TextView smallLadder;
+	TextView bigLadder;
 	TextView fullHouse;
 	TextView chance;
 	TextView yatzy;
 	TextView total;
+	Button done;
+	ImageView dice1;
+	ImageView dice2;
+	ImageView dice3;
+	ImageView dice4;
+	ImageView dice5;
+	Button throwButton;
 	
 		/** Called when the activity is first created.*/
 	@Override
@@ -42,6 +55,15 @@ public class GameActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game);
 		
+		for(int i = 0; i < 5; i++){
+			dice.add(i, new Dice());
+		}
+		
+		diceImages.add(dice1);
+		diceImages.add(dice2);
+		diceImages.add(dice3);
+		diceImages.add(dice4);
+		diceImages.add(dice5);
 		
 		/**
 		 * List with names of players to start game withf
@@ -53,27 +75,57 @@ public class GameActivity extends Activity {
 			players.add(i, new Player(playerNames.get(i)));
 		}
 		
-		TextView ones = (TextView) findViewById(R.id.ones);
-		TextView twos = (TextView) findViewById(R.id.twos);
-		TextView threes = (TextView) findViewById(R.id.threes);
-		TextView fours = (TextView) findViewById(R.id.fours);
-		TextView fives = (TextView) findViewById(R.id.fives);
-		TextView sixes = (TextView) findViewById(R.id.sixes);
-		TextView sum = (TextView) findViewById(R.id.sum);
-		TextView bonus = (TextView) findViewById(R.id.bonus);
-		TextView onePair = (TextView) findViewById(R.id.onePair);
-		TextView twoPair = (TextView) findViewById(R.id.twoPair);
-		TextView trips = (TextView) findViewById(R.id.trips);
-		TextView quads = (TextView) findViewById(R.id.quads);
-		TextView smallStraight = (TextView) findViewById(R.id.smallStraight);
-		TextView bigStraight = (TextView) findViewById(R.id.bigStraight);
-		TextView fullHouse = (TextView) findViewById(R.id.fullHouse);
-		TextView chance = (TextView) findViewById(R.id.chance);
-		TextView yatzy = (TextView) findViewById(R.id.yatzy);
-		TextView total = (TextView) findViewById(R.id.total);
+		gameText 	= (TextView) 	findViewById(R.id.headText);
+		playerName 	= (TextView) 	findViewById(R.id.playerNameText);
+		ones 		= (TextView) 	findViewById(R.id.ones);
+		twos 		= (TextView) 	findViewById(R.id.twos);
+		threes 		= (TextView) 	findViewById(R.id.threes);
+		fours 		= (TextView) 	findViewById(R.id.fours);
+		fives 		= (TextView) 	findViewById(R.id.fives);
+		sixes 		= (TextView) 	findViewById(R.id.sixes);
+		sum 		= (TextView) 	findViewById(R.id.sum);
+		bonus 		= (TextView) 	findViewById(R.id.bonus);
+		onePair 	= (TextView) 	findViewById(R.id.onePair);
+		twoPairs 	= (TextView) 	findViewById(R.id.twoPairs);
+		trips 		= (TextView) 	findViewById(R.id.trips);
+		quads 		= (TextView) 	findViewById(R.id.quads);
+		smallLadder = (TextView) 	findViewById(R.id.smallLadder);
+		bigLadder 	= (TextView) 	findViewById(R.id.bigLadder);
+		fullHouse 	= (TextView) 	findViewById(R.id.fullHouse);
+		chance 		= (TextView) 	findViewById(R.id.chance);
+		yatzy 		= (TextView) 	findViewById(R.id.yatzy);
+		total 		= (TextView) 	findViewById(R.id.total);
+		done		= (Button)		findViewById(R.id.doneButton);
+		dice1		= (ImageView)	findViewById(R.id.dice1);
+		dice2		= (ImageView)	findViewById(R.id.dice2);
+		dice3		= (ImageView)	findViewById(R.id.dice3);	
+		dice4		= (ImageView)	findViewById(R.id.dice4);
+		dice5		= (ImageView)	findViewById(R.id.dice5);
+		throwButton = (Button)		findViewById(R.id.throwButton);
 		
+		for(int i = 0; i < 5; i++){
+			
+			if(dice.get(i).getValue() == 1)
+				diceImages.get(i).setBackgroundResource(R.drawable.one);
+			if(dice.get(i).getValue() == 2)
+				diceImages.get(i).setBackgroundResource(R.drawable.two);
+			if(dice.get(i).getValue() == 3)
+				diceImages.get(i).setBackgroundResource(R.drawable.three);
+			if(dice.get(i).getValue() == 4)
+				diceImages.get(i).setBackgroundResource(R.drawable.four);
+			if(dice.get(i).getValue() == 5)
+				diceImages.get(i).setBackgroundResource(R.drawable.five);
+			if(dice.get(i).getValue() == 5)
+				diceImages.get(i).setBackgroundResource(R.drawable.six);
+		}
+		
+		throwButton.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				
+			}
+		}
 		CharSequence as = (CharSequence) playerNames.get(0);
-		test.setText(as);
+		//test.setText(as);
 	}
 	
 	public int roundCheck() {
